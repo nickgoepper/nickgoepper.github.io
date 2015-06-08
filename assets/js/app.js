731a92af6a83e403be52d6cc64333ee50ec6b353
mike-ward <mike@mike-ward.net>
2015-06-08 19:07:22 -0400

    site construction

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  alert("hello world");
+if(window.location.href=="http://nickgoepper.github.io/")
+{
+$("#sidebar").css({width:'100%'});
+$("#btnblog").click(function(){
+$("#sidebar").animate({width:'33.3333%'},'slow');
+});
+}
+});
