1e9393385aae97b3e7f82d5b7c5cbfb113df7b23
mike-ward <mike@mike-ward.net>
2015-06-08 19:16:18 -0400

    fix landing page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   alert("hello world");
-if(window.location.href=="http://nickgoepper.github.io/")
+if(window.location.href=="https://nickgoepper.github.io/")
 {
 $("#sidebar").css({width:'100%'});
 $("#btnblog").click(function(){
